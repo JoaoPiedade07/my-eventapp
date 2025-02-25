bc84fa95aa08e3ca0f2a0759ad24787af510958d
João Piedade <joaoluislopespiedade07@gmail.com>
2025-02-25 10:26:28 +0000

    Update login.tsx

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,10 +1,14 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Button, TextInput } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
 
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <ParallaxScrollView
             headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -17,6 +21,31 @@ export default function LoginScreen() {
             <ThemedView style={styles.titleContainer}>
                 <ThemedText type="title">Welcome to the Login Screen!</ThemedText>
             </ThemedView>
+
+            {/* Campos de entrada de dados */}
+
+            <ThemedView style={styles.formContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    placeholderTextColor="#888"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    placeholderTextColor="#888"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                />
+
+                <button title="Login" onProgress={ () => console.log(`Email: ${email}, Password: ${password}`)} />
+            </ThemedView>
         </ParallaxScrollView>
     );
 }
@@ -26,6 +55,10 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         gap: 8,
+    },
+    formContainer: {
+        padding: 16,
+        gap: 12,
     },
     stepContainer: {
         gap: 8,
@@ -37,5 +70,14 @@ const styles = StyleSheet.create({
         bottom: 0,
         left: 0,
         position: 'absolute',
+    },
+    input: {
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        fontSize: 16,
+        backgroundColor: '#fff',
     },
 });
